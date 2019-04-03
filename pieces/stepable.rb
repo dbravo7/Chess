7e20b627be908bc9b20f734a384ab40ac9ec3df1
David <dbravo7@yahoo.com>
2019-04-03 08:53:17 -0400

    Completed module

--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -1,8 +1,15 @@
 module Stepable
 
   def moves
-    
-
+    moves = []
+    move_diffs.each do |pos_x, pos_y|
+      x, y = pos 
+      new_move = [x + pos_x, y + pos_y]
+      if board[new_move].empty? || board[new_move].color != board[pos].color
+        moves << new_move 
+      end 
+    end 
+    moves 
   end 
 
   private
