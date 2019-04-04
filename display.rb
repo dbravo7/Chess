544b5e350f72404c5a611cd2127e4ace727d5bec
David <dbravo7@yahoo.com>
2019-04-04 16:06:25 -0400

    Added show_debug methods to display class

--- display.rb
+++ display.rb
@@ -9,8 +9,20 @@ attr_reader :cursor_pos, :board
   def initialize(board) 
     @cursor = Cursor.new([0,0], board)
     @board = board
+    @show_debug = true  
   end 
 
+  def toggle_debug
+    @show_debug == false ? true : false 
+  end 
+
+  def puts_debug
+    if @show_debug
+      puts "Available moves:#{@cursor.cursor_pos.moves}"
+      puts "Check? #{board.in_check?(@cursor.cursor_pos.color)}"
+    end 
+  end 
+  
   def render 
     system("clear")
     puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
@@ -30,6 +42,8 @@ attr_reader :cursor_pos, :board
       puts "#{x + 1}".colorize(:cyan) + "|" + rendered.join("")
     end 
     puts "   #{("a".."h").to_a.join("  ").reverse}".colorize(:cyan)
+    puts ""
+    puts_debug
     test_loop
   end 
 
